c4ddd80e6bd83a5414d0e7812347c6f85e83dd72
Jerry Zheng <jerry.zheng@hotmail.com>
2020-09-26 13:45:44 -0400

    extend box colliders in the z axis once the camera stops moving

---
+++ Assets/Scripts/Camera_Controller.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Controller : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Vector3 center;
+    public Vector3 orientation;
+    public Vector3 top_face;
+    public float scale;
+    public float speed;
+    public Camera c;
+    public bool isRotating;
+    
+    void Start()
+    {
+        center = new Vector3(1, 3, -5) ;
+        top_face = new Vector3(0, 1, 0);
+        scale = 10f;
+        speed = 30f;
+        orientation =  Vector3.forward;
+        transform.position = center + scale * orientation;
+        transform.LookAt(center);
+        isRotating = false;
+        scale_up_faces();
+        c = GetComponent<Camera>();
+    }
+
+    void rotate(Vector3 direction)
+    {
+        orientation = direction;
+        //transform.position = center + orientation;
+        //transform.LookAt(center);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float step = speed * Time.deltaTime; // calculate distance to move
+        transform.position = Vector3.MoveTowards(transform.position, center + scale * orientation, step);
+        transform.LookAt(center);
+
+
+        if (Input.inputString == "a")
+        {
+            if (transform.position == center + scale * orientation && isRotating == false)
+            {
+                scale_down_faces();
+                isRotating = true;
+            }
+            rotate(Vector3.right);
+        }
+
+        if (Input.inputString == "d")
+        {
+            if (transform.position == center + scale * orientation && isRotating == false)
+            {
+                scale_down_faces();
+                isRotating = true;
+            }
+            rotate(Vector3.forward);
+            
+        }
+
+        if (transform.position == center + scale * orientation && isRotating == true)
+        {
+           scale_up_faces();
+           isRotating = false;
+        }
+    }
+
+    void scale_up_faces(){
+        print("scale up");
+        GameObject cubes = GameObject.Find("Cubes");
+        BoxCollider[] boxColliders = cubes.GetComponentsInChildren<BoxCollider>();
+        foreach (BoxCollider boxCollider in boxColliders){
+            var s = boxCollider.size;
+            Vector3 factor = orientation;
+            factor *= 63;
+            factor += new Vector3(1, 1, 1);
+            s = new Vector3 ((factor.x)*s[0], (factor.y)*s[1], factor.z * s[2]);
+            boxCollider.size = s;
+        }
+    }
+    
+    void scale_down_faces(){
+        print("scale down");
+        GameObject cubes = GameObject.Find("Cubes");
+        BoxCollider[] boxColliders = cubes.GetComponentsInChildren<BoxCollider>();
+        foreach (BoxCollider boxCollider in boxColliders){
+            var s = boxCollider.size;
+            Vector3 factor = orientation;
+            factor *= 63;
+            factor += new Vector3(1, 1, 1);
+            s = new Vector3((factor.x)/s.x, (factor.y)/s.y, (factor.z)/s.z);
+            boxCollider.size = s;
+        }
+    }
+}
