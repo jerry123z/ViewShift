06e2ee0a4d28c14edf0ea7800376dc2cb95632df
hartand4 <andries.hart@mail.utoronto.ca>
2020-09-26 14:41:41 -0400

    gravity added

--- Assets/Scripts/PlayerMover.cs
+++ Assets/Scripts/PlayerMover.cs
@@ -11,13 +11,17 @@ public class PlayerMover : MonoBehaviour
     private Vector3 orientation;
     private Rigidbody rb;
     private SphereCollider sc;
+    private float distToGround;
 
     public float speed = 6f;
+
+    private float grav = -9.8f;
 
     private void Start()
     {
         rb = transform.GetComponent<Rigidbody>();
         sc = transform.GetComponent<SphereCollider>();
+        distToGround = sc.bounds.extents.y;
     }
 
     // Update is called once per frame
@@ -27,16 +31,20 @@ public class PlayerMover : MonoBehaviour
         print(temp);
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
+        
         Vector3 direction = new Vector3(-horizontal, 0f, vertical);
 
         if (direction.magnitude >= 0.1f)
         {
             controller.Move(direction * speed * Time.deltaTime);
         }
+        Vector3 vert = new Vector3(0f, grav, 0f);
+        controller.Move(vert * Time.deltaTime);
+
     }
 
     bool checkBottom()
     {
-        return Physics.Raycast(sc.bounds.center, Vector3.forward);
+        return Physics.Raycast(transform.position, -Vector3.up, (float)(distToGround+0.1));
     }
 }
