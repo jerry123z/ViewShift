9b5dabb0fef6f08fb6c07a7f1b7e74cc20032cbe
Leoncyao <leoncyao@gmail.com>
2020-10-16 10:54:50 -0700

    early iso demo

--- Assets/Scripts/Camera_Controller.cs
+++ Assets/Scripts/Camera_Controller.cs
@@ -23,21 +23,25 @@ public class Camera_Controller : MonoBehaviour
     //public GameObject test;
     void Start()
     {
-        print("level is " + Application.loadedLevelName);
+        //print("level is " + Application.loadedLevelName);
         center = new Vector3(1, 3, -5) ;
         top_face = new Vector3(0, 1, 0);
-        scale = 10f;
+        scale = 20f;
         speed = 30f;
-        orientation =  Vector3.forward;
+        orientation =  2 * Vector3.forward;
         transform.position = center + scale * orientation;
         transform.LookAt(center);
         isRotating = false;
+        up = Vector3.up + Vector3.right;
+        current_up = up;
         scale_up_faces();
         c = GetComponent<Camera>();
 
-        up = Vector3.up;
-        current_up = up;
 
+        //print("orientation is " + orientation);
+        //print("up is " + up);
+        //print("cross product is  " + Vector3.Cross(orientation,up));
+        //print("up is " + up);
         //test.transform.LookAt(up);
     }
 
@@ -58,8 +62,8 @@ public class Camera_Controller : MonoBehaviour
     {
 
         float step = speed * Time.deltaTime; // calculate distance to move
-        transform.position = Vector3.MoveTowards(transform.position, center + scale * orientation, step);
-        transform.LookAt(center, current_up);
+        transform.position = Vector3.MoveTowards(transform.position, center + scale * (orientation + Vector3.up + Vector3.right), step);
+        transform.LookAt(center, current_up + scale * Vector3.up);
 
 
 
@@ -68,9 +72,88 @@ public class Camera_Controller : MonoBehaviour
         current_up = Vector3.MoveTowards(current_up, up, 1 * Time.deltaTime);
 
 
+        if (Application.loadedLevelName != "level4")
+        {
+            if (Input.inputString == "j")
+            {
+                //print("center is " + center);
+                //print("up is " + up);
 
+                GameObject parent = player.GetComponent<PlayerMover>().touching;
+                //print("parent tag" + parent.tag);
+                if (parent.tag != "column")
+                {
+                    //print("forward is " + parent.transform.forward);
+                    //print("current position is" + player.transform.position);
+                    //print("calculating using parent pos" + parent.transform.position);
+                    //print("calculating using parent forward" + parent.transform.forward);
+                    //print("now it is " + player.transform.position);
+                    player.transform.position = parent.transform.position + parent.transform.forward;
+                    player.transform.SetParent(parent.transform, true);
+                    up = Quaternion.Euler(0, 0, -90) * up;
+                }
+                //print(up);
+            }
+            if (Input.inputString == "l")
+            {
+                GameObject parent = player.GetComponent<PlayerMover>().touching;
+                if (parent.tag != "column")
+                {
+                    player.transform.position = parent.transform.position + parent.transform.forward;
+                    player.transform.SetParent(parent.transform, true);
+                    Physics.gravity = -parent.transform.forward * 9.8f;
+                    up = Quaternion.Euler(0, 0, 90) * up;
+                }
+            }
 
-        if (Application.loadedLevelName != "level3")
+            if (current_up != up)
+            {
+                //print(current_up);
+
+                player.GetComponent<Rigidbody>().useGravity = false;
+                GameObject parent = player.GetComponent<PlayerMover>().touching;
+                if (parent)
+                {
+                    //player.transform.parent = parent.transform;
+                    //player.transform.localPosition = current_up;
+                    Physics.gravity = -parent.transform.forward * 9.8f;
+                    parent.transform.LookAt(parent.transform.position + current_up);
+                }
+            }
+            else
+            {
+                player.GetComponent<Rigidbody>().useGravity = true;
+            }
+            if (Input.inputString == "a")
+            {
+                if (transform.position == center + scale * (orientation + Vector3.up + Vector3.right) && isRotating == false)
+                {
+                    scale_down_faces();
+                    isRotating = true;
+                }
+                rotate(Vector3.left);
+                player.GetComponent<PlayerMover>().snap();
+
+            }
+
+            if (Input.inputString == "d")
+            {
+
+                if (transform.position == center + scale * (orientation + Vector3.up + Vector3.right) && isRotating == false)
+                {
+                    scale_down_faces();
+                    isRotating = true;
+                }
+                rotate(Vector3.right);
+                player.GetComponent<PlayerMover>().snap();
+            }
+
+            if (transform.position == center + scale * (orientation + Vector3.up + Vector3.right) && isRotating == true)
+            {
+                scale_up_faces();
+                isRotating = false;
+            }
+        } else if (Application.loadedLevelName != "level3")
         {
             if (Input.inputString == "a")
             {
@@ -161,20 +244,26 @@ public class Camera_Controller : MonoBehaviour
 
     void scale_up_faces(){
         //print("scale up");
+        print("orientation is " + orientation);
+        print("up is " + up);
+        print("factor is " + Vector3.Cross(orientation, up));
         GameObject cubes = GameObject.Find("Cubes");
         BoxCollider[] boxColliders = cubes.GetComponentsInChildren<BoxCollider>();
         foreach (BoxCollider boxCollider in boxColliders){
             var s = boxCollider.size;
-            Vector3 factor = orientation;
+            //Vector3 factor = orientation;
+            Vector3 factor = Vector3.Cross(orientation, up);
             factor *= 63;
             factor += new Vector3(1, 1, 1);
             if (boxCollider.material.name == "testmaterial (Instance)")
             {
-                s = new Vector3((factor.x) * s[0], (factor.y) * s[1], 1);
+                s = new Vector3((Mathf.Abs(factor.x)) * s[0], Mathf.Abs(factor.y) * s[1], 1);
             }
             else
             {
-                s = new Vector3((factor.x) * s[0], (factor.y) * s[1], factor.z * s[2]);
+                //s = new Vector3(Mathf.Abs(factor.x) * s[0], Mathf.Abs(factor.y) * s[1], Mathf.Abs(factor.z) * s[2]);
+                s = new Vector3(Mathf.Abs(factor.x) * s[0], Mathf.Abs(factor.y) * s[1], Mathf.Abs(factor.z) * s[2]);
+                print("s is " + s);
             }
             boxCollider.size = s;
         }
@@ -184,20 +273,23 @@ public class Camera_Controller : MonoBehaviour
         //print("scale down");
         //GameObject cubes = GameObject.Find("Cubes");
         BoxCollider[] boxColliders = cubes.GetComponentsInChildren<BoxCollider>();
+        Vector3 factor = Vector3.Cross(orientation, up);
+        print("factor is" + factor);
         foreach (BoxCollider boxCollider in boxColliders){
             var s = boxCollider.size;
-            Vector3 factor = orientation;
+            //Vector3 factor = orientation;
+            //Vector3 factor = Vector3.Cross(orientation, up);
             factor *= 63;
             factor += new Vector3(1, 1, 1);
-            print(boxCollider.material.name);
+            //print(boxCollider.material.name);
             if (boxCollider.material.name == "testmaterial (Instance)")
             {
-                print("checked");
-                s = new Vector3((factor.x) / s.x, (factor.y) / s.y, 1);
+                //print("checked");
+                s = new Vector3(Mathf.Abs(factor.x) / s.x, Mathf.Abs(factor.y) / s.y, 1);
             }
             else
             {
-                s = new Vector3((factor.x) / s.x, (factor.y) / s.y, (factor.z) / s.z);
+                s = new Vector3(Mathf.Abs(factor.x) / s.x, Mathf.Abs(factor.y) / s.y, Mathf.Abs(factor.z) / s.z);
             }
             boxCollider.size = s;
         }
