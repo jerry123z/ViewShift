9d5a2d73fd3882c53c3b5a15a8b8117638a19532
hartand4 <andries.hart@mail.utoronto.ca>
2020-11-01 01:42:34 -0400

    copied demoScene2 to work on robot character model/animations

--- Assets/Scripts/Camera_Controller.cs
+++ Assets/Scripts/Camera_Controller.cs
@@ -63,7 +63,7 @@ public class Camera_Controller : MonoBehaviour
             //transform.position = Vector3.MoveTowards(transform.position, height + center.position +  scale * (isometricOffset * orientation), step);
         } else{
             //print("isRotating = false");
-            player.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
+            player.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotation;
             float step = speed * 20 * Time.deltaTime; // calculate distance to move
             transform.position = Vector3.MoveTowards(transform.position, height + center.position +  scale * (isometricOffset * orientation), step);
         }
