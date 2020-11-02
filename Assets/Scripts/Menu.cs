4566f847d2aa6b27535bb8fb7628e843cdfedb02
Jerry Zheng <jerry.zheng@hotmail.com>
2020-11-01 22:57:27 -0500

    reverting andries' commit

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public void restartGame()
+    {
+        SceneManager.LoadScene("demoScene2");
+    }
+
+    public void returnMain()
+    {
+        SceneManager.LoadScene("Title Scene");
+    }
+
+    public void quit()
+    {
+        Application.Quit();
+    }
+}
