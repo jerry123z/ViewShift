ffbee34adcef727240e80170e1468e782c44845a
Jerry Zheng <jerry.zheng@hotmail.com>
2020-11-01 21:03:30 -0500

    moved level loading into another script

---
+++ Assets/Scripts/LoadLevelCollider.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadLevelCollider:MonoBehaviour
+{
+    public int level;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene(level);
+        }
+    }
+}
