49cdcf640bc2d33ca5144f97da51fde65d7bf7de
zkjfry <jeffreychen0826@gmail.com>
2020-11-04 23:14:16 +0800

    added functionality for option as the keymapping page

--- Assets/Scripts/TitleScreenController.cs
+++ Assets/Scripts/TitleScreenController.cs
@@ -5,9 +5,23 @@ using UnityEngine.SceneManagement;
 
 public class TitleScreenController : MonoBehaviour
 {
+    private bool state;
+    private GameObject keyMap;
+
+    private void Start()
+    {
+        keyMap = GameObject.Find("/Canvas/KeyMapping");
+        keyMap.SetActive(false);
+        state = false;
+    }
     public void startGame()
     {
         SceneManager.LoadScene("AlphaLevel");
+    }
+
+    public void option()
+    {
+        keyMap.SetActive(!state);
     }
 
     public void quitGame()
