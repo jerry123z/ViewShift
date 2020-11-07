c26c70e9881da5ae8b493f9a725841320506c5e3
hartand4 <andries.hart@mail.utoronto.ca>
2020-11-07 15:09:45 -0500

    Added camera effects: ambient occlusion, motion blur when rotating

---
+++ Assets/Scripts/TiltShift.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+public class TiltShift : MonoBehaviour
+{
+    private GameObject camera;
+    private MotionBlur motionBlurLayer;
+
+    void Start()
+    {
+        camera = GameObject.Find("Main Camera");
+        PostProcessVolume vol = transform.GetComponent<PostProcessVolume>();
+        vol.profile.TryGetSettings(out motionBlurLayer);
+    }
+
+    void Update()
+    {
+        Camera_Controller cameraController = camera.GetComponent<Camera_Controller>();
+        if (cameraController.isRotating)
+        {
+            motionBlurLayer.enabled.value = true;
+        }
+        else
+        {
+            motionBlurLayer.enabled.value = false;
+        }
+    }
+}
