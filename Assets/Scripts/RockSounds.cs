a6c64b14a3d95cf1a14adc899227aea52a87ebf0
hartand4 <andries.hart@mail.utoronto.ca>
2020-12-08 00:39:34 -0500

    changed the ground in the ramp hill level

---
+++ Assets/Scripts/RockSounds.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RockSounds : MonoBehaviour
+{
+
+    private GameObject cam;
+    public AudioClip slide;
+    public bool playing;
+    private AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = GameObject.Find("Main Camera");
+        playing = false;
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (cam.GetComponent<Camera_Controller>().isRotating &&
+        GetComponent<RelativeRotatorData>().willRotate && !playing)
+        {
+            playing = true;
+            audioSource.PlayOneShot(slide, 0.5f);
+        }
+        if (!cam.GetComponent<Camera_Controller>().isRotating)
+        {
+            playing = false;
+        }
+    }
+}
